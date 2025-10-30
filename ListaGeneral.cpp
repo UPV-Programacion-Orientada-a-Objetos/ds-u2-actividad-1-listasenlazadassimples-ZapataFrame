fc85cdda521197326762044914d3ffa79e3e0a2a
Diego Zapata <2430053@upv.edu.mx>
2025-10-30 17:20:40 -0600

    Agregar Lista general

---
+++ ListaGeneral.cpp
@@ -0,0 +1,86 @@
+#include "ListaGeneral.h"
+#include <iostream>
+// implementacion
+
+ListaGeneral::~ListaGeneral()
+{
+    // si es null no se libera nada
+    if (this->cabeza == nullptr)
+        return;
+    Nodo<SensorBase *> *temp = this->cabeza;
+    while (temp != nullptr)
+    {
+        Nodo<SensorBase *> *aux = temp->siguiente;
+        delete temp->valor;
+        delete temp;
+        temp = aux;
+    }
+    this->cabeza = nullptr;
+}
+
+void ListaGeneral::insertar(SensorBase *valor)
+{
+    Nodo<SensorBase *> *nuevoNodo = new Nodo<SensorBase *>(valor);
+    if (this->cabeza == nullptr)
+    {
+        this->cabeza = nuevoNodo;
+    }
+    else
+    {
+        Nodo<SensorBase *> *temp = this->cabeza;
+        while (temp->siguiente != nullptr)
+        {
+            temp = temp->siguiente;
+        }
+        temp->siguiente = nuevoNodo;
+    }
+}
+bool ListaGeneral::estaVacia()
+{
+    return cabeza == nullptr;
+}
+
+void ListaGeneral::procesarTodos()
+{
+    if (this->cabeza == nullptr)
+    {
+        std::cout << "No hay sensores para procesar." << std::endl;
+        return;
+    }
+
+    std::cout << "\n--- Ejecutando Polimorfismo ---" << std::endl;
+
+    Nodo<SensorBase *> *temp = this->cabeza;
+    while (temp != nullptr)
+    {
+        std::cout << "-> Procesando sensor ";
+        temp->valor->printNombre();
+        std::cout << std::endl;
+
+        temp->valor->procesarLectura();
+        temp = temp->siguiente;
+        std::cout << std::endl;
+    }
+}
+
+void ListaGeneral::imprimirTodos() const
+{
+    if (this->cabeza == nullptr)
+    {
+        std::cout << "No hay sensores registrados." << std::endl;
+        return;
+    }
+
+    std::cout << "\n--- Lista de Sensores ---" << std::endl;
+
+    Nodo<SensorBase *> *temp = this->cabeza;
+    int contador = 1;
+
+    while (temp != nullptr)
+    {
+        std::cout << "\n[Sensor " << contador << "]" << std::endl;
+        temp->valor->imprimirInfo();
+        temp = temp->siguiente;
+        contador++;
+    }
+}
---
+++ ListaGeneral.h
@@ -0,0 +1,21 @@
+#ifndef LISTAGENERAL_H
+#define LISTAGENERAL_H
+
+#include "Nodo.h"
+#include "SensorBase.h"
+
+// interfaz
+class ListaGeneral
+{
+private:
+    Nodo<SensorBase *> *cabeza;
+
+public:
+    ListaGeneral() : cabeza(nullptr) {};
+    ~ListaGeneral();
+    void insertar(SensorBase *valor);
+    bool estaVacia();
+    void procesarTodos();
+    void imprimirTodos() const;
+};
+#endif
